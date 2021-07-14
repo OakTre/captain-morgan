06f08e6af02705e2f866d75f482a846f6d441ebf
b2b <mail@b2b.ru>
2021-07-14 16:00:06 +0300

    тест на хостинге

--- src/js/blocks/videoInit.js
+++ src/js/blocks/videoInit.js
@@ -1,12 +1,82 @@
+import gsap from "gsap";
+
+
 export default () => {
-	const button = document.querySelector(".morgan-hero__button");
+	const buttonHero = document.querySelector(".morgan-hero__button");
 	const video = document.querySelector(".morgan-hero__video");
 	const info = document.querySelector(".morgan-hero__info");
+	const title = document.querySelector(".morgan-hero__title");
+	const playButton = document.querySelector(".play-button-js");
 
-	button.addEventListener("click", function(){
+	let currentStep = 'first';
+	let steps = {
+		first: {
+			time: 4.77,
+			nextStep: 'second'
+		},
+		second: {
+			time: 11.29,
+			nextStep: 'third'
+		},
+		third: {
+			time: 16.85,
+			nextStep: 'thourth'
+		},
+		thourth: {
+			time: 20.71,
+			nextStep: 'fifth'
+		},
+		fifth: {
+			time: 100,
+			nextStep: 'end'
+		}
+	};
+
+	playButton.addEventListener("click", function(){
 		video.play();
-
-		info.style.display = "none";
 	});
+
+	buttonHero.addEventListener("click", function(){
+
+		gsap.to(title, {
+			y: "-100%",
+			duration: 0.8
+		})
+
+		this.classList.add("_active");
+
+		video.play();
+	});
+
+
+	video.addEventListener("timeupdate", function(){
+		let curTime = this.currentTime;
+
+		if (currentStep) {
+
+			if (curTime > steps[currentStep].time) {
+			  currentStep = steps[currentStep].nextStep;
+
+			  document.querySelector(`.morgan-title[data-text="${currentStep}"]`).classList.add("_active");
+
+			  hideText(currentStep);
+
+			  this.pause();
+			}
+
+		} else {
+			video.stop();
+		}
+	});
+
+	function hideText(step) {
+		const parent = document.querySelector(`.morgan-title[data-text="${step}"]`);
+		const currentButton = parent.querySelector(".main-button");
+
+		currentButton.addEventListener("click", function(){
+			parent.classList.remove("_active");
+			video.play();
+		});
+	}
 
 }
